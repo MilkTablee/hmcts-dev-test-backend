a072911d2854da287c889dab4e65016328bf444c
MilkTablee <119823959+MilkTablee@users.noreply.github.com>
2025-04-15 17:51:52 +0100

    Add Task model with fields for case number, title, description, dates, and status

---
+++ src/main/java/uk/gov/hmcts/reform/dev/controllers/TaskController.java
@@ -0,0 +1,2 @@
+package uk.gov.hmcts.reform.dev.controllers;public class TaskController {
+}
--- src/main/java/uk/gov/hmcts/reform/dev/models/Task.java
+++ src/main/java/uk/gov/hmcts/reform/dev/models/Task.java
@@ -1,2 +1,23 @@
-package uk.gov.hmcts.reform.dev.models;public class Task {
+package uk.gov.hmcts.reform.dev.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Task {
+
+    private int id;
+    private String caseNumber;
+    private String title;                   // Task title
+    private String description;             // Task description
+    private LocalDateTime dueDate;          // Due date for the task
+    private String status;                  // Task status (e.g., "in progress", "completed")
+    private LocalDateTime createdDate;      // Task creation date
 }
